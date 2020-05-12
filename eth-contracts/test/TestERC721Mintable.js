e3b5590509f23f150fcaa342458d349dcdf15cec
Shuda Mo <msdtim@gmail.com>
2020-05-12 15:05:13 +0800

    feat: Impletement CustomERC721Token

--- eth-contracts/test/TestERC721Mintable.js
+++ eth-contracts/test/TestERC721Mintable.js
@@ -10,6 +10,7 @@ contract('TestERC721Mintable', accounts => {
             this.contract = await ERC721MintableComplete.new({from: account_one});
 
             // TODO: mint multiple tokens
+            
         })
 
         it('should return total supply', async function () { 
