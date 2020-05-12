730ed255f583488ca05d31f420bf35470af9e796
Shuda Mo <msdtim@gmail.com>
2020-05-12 20:12:40 +0800

    feat: Compile and test verifier

--- eth-contracts/migrations/2_deploy_contracts.js
+++ eth-contracts/migrations/2_deploy_contracts.js
@@ -1,11 +1,11 @@
 // migrating the appropriate contracts
-//var SquareVerifier = artifacts.require("./SquareVerifier.sol");
+var SquareVerifier = artifacts.require("./SquareVerifier.sol");
 //var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
 var CustomERC721Token = artifacts.require('CustomERC721Token');
 
 
 module.exports = function(deployer) {
-  //deployer.deploy(SquareVerifier);
+  deployer.deploy(SquareVerifier);
   //deployer.deploy(SolnSquareVerifier);
   deployer.deploy(CustomERC721Token);
 };
